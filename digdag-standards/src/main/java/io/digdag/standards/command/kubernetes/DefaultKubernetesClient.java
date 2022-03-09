3b6e3d5bea30046a61f543cb23815df46bec2235
Kyou13 <kyou1013@gmail.com>
2022-03-09 16:09:26 +0900

    update kubernetes-client version

--- digdag-standards/src/main/java/io/digdag/standards/command/kubernetes/DefaultKubernetesClient.java
+++ digdag-standards/src/main/java/io/digdag/standards/command/kubernetes/DefaultKubernetesClient.java
@@ -6,11 +6,13 @@ import com.google.common.io.CharStreams;
 import io.digdag.client.config.Config;
 import io.digdag.spi.CommandContext;
 import io.digdag.spi.CommandRequest;
+import io.fabric8.kubernetes.api.model.ConfigMapBuilder;
 import io.fabric8.kubernetes.api.model.Container;
 import io.fabric8.kubernetes.api.model.ContainerBuilder;
 import io.fabric8.kubernetes.api.model.ContainerStatus;
 import io.fabric8.kubernetes.api.model.EnvVar;
 import io.fabric8.kubernetes.api.model.EnvVarBuilder;
+import io.fabric8.kubernetes.api.model.PodBuilder;
 import io.fabric8.kubernetes.api.model.PodSpec;
 import io.fabric8.kubernetes.api.model.PodSpecBuilder;
 import io.fabric8.kubernetes.api.model.Quantity;
@@ -51,15 +53,16 @@ public class DefaultKubernetesClient
     {
         final Container container = createContainer(context, request, name, commands, arguments);
         final PodSpec podSpec = createPodSpec(context, request, container);
-        final io.fabric8.kubernetes.api.model.Pod pod = client.pods()
-                .inNamespace(client.getNamespace())
-                .createNew()
-                .withNewMetadata()
-                .withName(name)
-                .withLabels(getPodLabels())
-                .endMetadata()
-                .withSpec(podSpec)
-                .done();
+        final io.fabric8.kubernetes.api.model.Pod pod = client.pods().inNamespace(client.getNamespace()).create(
+               new PodBuilder()
+                   .withNewMetadata()
+                   .withName(name)
+                   .withLabels(getPodLabels())
+                   .withNamespace(client.getNamespace())
+                   .endMetadata()
+                   .withSpec(podSpec)
+                   .build()
+               );
         return Pod.of(pod);
     }
 
--- digdag-standards/src/main/java/io/digdag/standards/command/kubernetes/DefaultKubernetesClientFactory.java
+++ digdag-standards/src/main/java/io/digdag/standards/command/kubernetes/DefaultKubernetesClientFactory.java
@@ -2,6 +2,8 @@ package io.digdag.standards.command.kubernetes;
 
 import io.fabric8.kubernetes.client.Config;
 import io.fabric8.kubernetes.client.ConfigBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DefaultKubernetesClientFactory
         implements KubernetesClientFactory
@@ -9,12 +11,14 @@ public class DefaultKubernetesClientFactory
     @Override
     public KubernetesClient newClient(final KubernetesClientConfig kubernetesClientConfig)
     {
+        Logger logger = LoggerFactory.getLogger(DefaultKubernetesClientFactory.class);
         final Config clientConfig = new ConfigBuilder()
                 .withMasterUrl(kubernetesClientConfig.getMaster())
                 .withNamespace(kubernetesClientConfig.getNamespace())
                 .withCaCertData(kubernetesClientConfig.getCertsCaData())
                 .withOauthToken(kubernetesClientConfig.getOauthToken())
                 .build();
+        logger.debug("###clientConfig:{}",clientConfig);
         return new DefaultKubernetesClient(kubernetesClientConfig,
                 new io.fabric8.kubernetes.client.DefaultKubernetesClient(clientConfig));
     }
